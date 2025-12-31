c7da9f72803b510375964805bb210c6a9684e32a
roney ruiz <roneyruizrojas@gmail.com>
2025-12-30 20:44:47 -0600

    feat: create a little example of http

--- server/src/Entrypoint.ts
+++ server/src/Entrypoint.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const expr = express();
+
+expr.get("/", (req, res) => {
+  console.log(req.ip);
+
+  res.status(200).json({
+    message: "Hello World!",
+  });
+});
+
+expr.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
