31939e2e53094ad14b80c3bedbb31cb91c417ff7
roney ruiz <roneyruizrojas@gmail.com>
2026-01-02 22:23:36 -0600

    feat: create a simple client sockt example

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
-  
+import { Socket } from "@Services/Socket";
+import { onMounted, onUnmounted } from "vue";
+
+onMounted(() => {
+  Socket.connect();
+});
+
+onUnmounted(() => {
+  Socket.disconnect();
+});
 </script>
 
 <template>
-  <div class="text-3xl font-bold underline">
-    <p>Hello World!!!</p>
+  <div class="min-h-screen bg-black">
+
   </div>
 </template>
-
