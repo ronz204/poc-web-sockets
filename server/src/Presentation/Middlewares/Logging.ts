44a53cc58ae03e90c5ed18d95bf65aa1f335447a
roney ruiz <roneyruizrojas@gmail.com>
2026-01-08 21:01:25 -0600

    feat: create simple logging middleware

---
+++ server/src/Presentation/Middlewares/Logging.ts
@@ -0,0 +1,10 @@
+import type { Request, Response, NextFunction } from 'express';
+
+export const LoggingMiddleware = (
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
