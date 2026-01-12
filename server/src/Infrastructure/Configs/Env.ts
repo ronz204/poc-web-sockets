8baa7f7509e1765da16881bd513c419a97b17211
roney ruiz <roneyruizrojas@gmail.com>
2026-01-11 20:59:35 -0600

    feat: rename environment and add vars

--- server/src/Infrastructure/Configs/Env.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod";
-
-const envSchema = z.object({
-  PORT: z.coerce.number().int().min(1),
-  ALLOWED_ORIGINS: z.string().min(1),
-});
-
-export const Env = envSchema.parse(process.env);
---
+++ server/src/Infrastructure/Configs/Environment.ts
@@ -0,0 +1,16 @@
+import * as z from "zod";
+
+const envSchema = z.object({
+  // App configs
+  PORT: z.coerce.number().int().min(1),
+
+  // CORS configs
+  ALLOWED_ORIGINS: z.string().min(1),
+
+  // Database configs
+  POSTGRES_DB: z.string().min(1),
+  POSTGRES_USER: z.string().min(1),
+  POSTGRES_PASSWORD: z.string().min(1),
+});
+
+export const Environment = envSchema.parse(process.env);
