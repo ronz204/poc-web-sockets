c4327743a7189710eec43332c5870f1267c0b574
roney ruiz <roneyruizrojas@gmail.com>
2026-01-11 20:40:37 -0600

    chore: rename postgres conn string

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env("POSTGRES_URL"),
   },
 });
--- server/src/Infrastructure/Database/Prisma.ts
+++ server/src/Infrastructure/Database/Prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@Generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.POSTGRES_URL!,
 });
 
 export const Prisma = new PrismaClient({ adapter });
