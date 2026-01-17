0fed8c649004d4670a6b42574bd36e392b9ee576
roney ruiz <roneyruizrojas@gmail.com>
2026-01-16 21:33:42 -0600

    chore: remove prisma related files

--- server/src/Infrastructure/Configs/PrismaConfig.ts
+++
@@ -1,6 +0,0 @@
-import { Environment } from "./Environment";
-
-export const PrismaConfig = {
-  db: Environment.POSTGRES_DB,
-  url: Environment.POSTGRES_URL,
-};
--- server/src/Infrastructure/Database/Prisma.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@Generated/prisma/client";
-import { PrismaConfig } from "@Configs/PrismaConfig";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const adapter = new PrismaPg({
-  connectionString: PrismaConfig.url,
-});
-
-export const Prisma = new PrismaClient({ adapter });
