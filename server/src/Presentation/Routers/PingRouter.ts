4ef662b37304eb29671f0842e5ae9756b475e46c
roney ruiz <roneyruizrojas@gmail.com>
2026-01-17 23:08:42 -0600

    chore: purge router components

--- server/src/Presentation/Routers/PingRouter.ts
+++
@@ -1,5 +0,0 @@
-import Elysia from "elysia";
-import { PingController } from "@Controllers/PingController";
-
-export const PingRouter = new Elysia({ prefix: "/ping" });
-PingRouter.get("/", () => PingController.ping());
