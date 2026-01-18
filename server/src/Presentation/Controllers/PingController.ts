d72069e2c745cd62ac070328ea6d26e25d021520
roney ruiz <roneyruizrojas@gmail.com>
2026-01-17 23:38:09 -0600

    fix: put logger in global scope

--- server/src/Presentation/Controllers/PingController.ts
+++ server/src/Presentation/Controllers/PingController.ts
@@ -1,6 +1,6 @@
 import Elysia from "elysia";
 
-export const PingController = new Elysia({ prefix: "/ping" })
+export const PingController = new Elysia({ prefix: "/ping", name: "ping-controller" })
   .get("/", () => {
     return { message: "pong" };
   });
--- server/src/Presentation/Middlewares/LoggerMiddleware.ts
+++ server/src/Presentation/Middlewares/LoggerMiddleware.ts
@@ -3,7 +3,6 @@ import Elysia from "elysia";
 export const LoggerMiddleware = new Elysia({ name: "logger" })
   .onRequest(({ request }) => {
     console.log(`Request: ${request.method} ${new URL(request.url).pathname}`);
-  })
-  .onAfterResponse(({ request }) => {
+  }).onAfterResponse({ as: "global" }, ({ request }) => {
     console.log(`Response: ${request.method} ${new URL(request.url).pathname}`);
   });
