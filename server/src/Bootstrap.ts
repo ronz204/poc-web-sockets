018bf4733e25944a5a58655f49dd50ea2018eaa6
roney ruiz <roneyruizrojas@gmail.com>
2026-01-19 21:31:39 -0600

    feat: setup basic cors config

--- server/src/Bootstrap.ts
+++ server/src/Bootstrap.ts
@@ -1,10 +1,15 @@
 import Elysia from "elysia";
+import cors from "@elysiajs/cors";
 
 // ==== Controllers ====
 import { AuthController } from "@Controllers/AuthController";
 
 // ==== Middlewares ====
 import { LoggingMiddleware } from "@Middlewares/LoggingMiddleware";
+
+const addCors = (app: Elysia) => {
+  return app.use(cors({ origin: "*" }));
+};
 
 const addControllers = (app: Elysia) => {
   return app
@@ -18,6 +23,7 @@ const addMiddlewares = (app: Elysia) => {
 
 export const Bootstrap = (app: Elysia) => {
   return app
+    .use(addCors)
     .use(addMiddlewares)
     .use(addControllers);
 };
