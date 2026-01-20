a5ccbde1ca1d862c2e12143a1d95de3155b7227b
roney ruiz <roneyruizrojas@gmail.com>
2026-01-19 21:16:28 -0600

    feat: implement sub elysias

--- server/src/Entrypoint.ts
+++ server/src/Entrypoint.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
+import { AuthController } from "@Controllers/AuthController";
+import { LoggingMiddleware } from "@Middlewares/LoggingMiddleware";
 
 const app = new Elysia()
+  .use(AuthController)
+  .use(LoggingMiddleware)
   .listen(3000);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
