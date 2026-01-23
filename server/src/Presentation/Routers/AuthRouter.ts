3899c16fe46b58ae68c86c240bcad625895647ad
roney ruiz <roneyruizrojas@gmail.com>
2026-01-22 20:34:15 -0600

    chore: purge old bad named files

--- server/src/Presentation/Routers/AuthRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Elysia, type ElysiaConfig } from "elysia";
-
-const config: ElysiaConfig<"/auth"> = {
-  prefix: "/auth", name: "auth-router"
-};
-
-export const AuthRouter = new Elysia(config)
-  .get("/health", () => ({ status: "healthy" }));
