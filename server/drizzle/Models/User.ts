fa7b9deda7aa5d3468a8d4e8e9d6d5bfb5854f28
roney ruiz <roneyruizrojas@gmail.com>
2026-02-23 21:06:59 -0600

    chore(drizzle): rename models and migrations folders

---
+++ server/drizzle/Models/User.ts
@@ -0,0 +1,8 @@
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const Users = pgTable("users", (t) => ({
+  id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: t.varchar().notNull().unique(),
+  email: t.varchar().notNull().unique(),
+  password: t.varchar().notNull(),
+}));
