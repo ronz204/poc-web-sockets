3ec6f85303537df6dedfc9e19b2229dd00a98c3c
roney ruiz <roneyruizrojas@gmail.com>
2026-02-23 21:06:04 -0600

    chore(drizzle): remove migrations and models temporary

--- server/drizzle/models/User.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable } from "drizzle-orm/pg-core";
-
-export const Users = pgTable("users", (t) => ({
-  id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: t.varchar().notNull().unique(),
-  email: t.varchar().notNull().unique(),
-  password: t.varchar().notNull(),
-}));
