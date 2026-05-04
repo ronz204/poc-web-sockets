fdf7ba959be34264b8274697007aa2f01497b337
roney ruiz <roneyruizrojas@gmail.com>
2026-05-03 20:38:51 -0600

    feat(server): setup simple boot entrypoint

--- server/source/boot.ts
+++ server/source/boot.ts
@@ -1,1 +1,14 @@
-console.log("hello from elysia");
+import { env } from "@env";
+import { Elysia } from "elysia";
+import { CorsPlugin } from "@plugins/cors.plugin";
+import { HealthPlugin } from "@plugins/health.plugin";
+import { ScalarPlugin } from "@plugins/scalar.plugin";
+
+export const app = new Elysia({ prefix: "/api" })
+  .use(CorsPlugin)
+  .use(ScalarPlugin)
+  .use(HealthPlugin)
+  .listen(env.APP_PORT);
+
+const url = `http://${app.server?.hostname}:${app.server?.port}`;
+console.log(`🦊 Elysia is running at ${url}`);
