74812e2ae5b31c7eeb2996f4f2a8ff2d27496a17
roney ruiz <roneyruizrojas@gmail.com>
2026-05-03 20:34:15 -0600

    feat(server): create env global and safe access

---
+++ server/source/env.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  // ==========================================
+  // Application Config
+  // ==========================================
+  APP_NAME: z.string().default("Sockets API"),
+  APP_PORT: z.string().transform((val) => parseInt(val, 10)).default(3000),
+
+  // ==========================================
+  // PostgreSQL Config
+  // ==========================================
+  POSTGRES_DB: z.string(),
+  POSTGRES_URL: z.string(),
+  POSTGRES_HOST: z.string(),
+  POSTGRES_PORT: z.string().transform((val) => parseInt(val, 10)).default(5432),
+
+  // ==========================================
+  // Redis & Cache Config
+  // ==========================================
+  REDIS_URL: z.string(),
+  REDIS_HOST: z.string(),
+  REDIS_PORT: z.string().transform((val) => parseInt(val, 10)).default(6379),
+
+  // ==========================================
+  // Security Config
+  // ==========================================
+  SECRET_KEY: z.string(),
+  ACCESS_TTL: z.string().default("15m"),
+  REFRESH_TTL: z.number().default(7 * 24 * 60 * 60),
+  CORS_ORIGIN: z.string().transform((val) => val.split(",").map((origin) => origin.trim())),
+});
+
+export const env = envSchema.parse(process.env);
