5c2ebba085cd4360c6700a68da99a6bd844e5a35
roney ruiz <roneyruizrojas@gmail.com>
2026-05-04 19:31:45 -0600

    fix(server): return type in user idao methods

--- server/source/core/dal/users/user.dao.ts
+++ server/source/core/dal/users/user.dao.ts
@@ -1,3 +1,4 @@
+import type { User } from "@prisma/client";
 import type { IUserDao } from "./user.idao";
 import { PrismaClient } from "@prisma/client";
 
@@ -9,16 +10,16 @@ import { Obtain } from "./queries/obtain.query";
 export class UserDao implements IUserDao {
   public constructor(private readonly prisma: PrismaClient) {};
 
-  public async create(args: Create.Args): Promise<void> {
-    await this.prisma.user.create(Create.query(args));
+  public async create(args: Create.Args): Promise<User> {
+    return await this.prisma.user.create(Create.query(args));
   };
 
-  public async delete(args: Delete.Args): Promise<void> {
-    await this.prisma.user.delete(Delete.query(args));
+  public async delete(args: Delete.Args): Promise<User> {
+    return await this.prisma.user.delete(Delete.query(args));
   };
 
-  public async update(args: Update.Args): Promise<void> {
-    await this.prisma.user.update(Update.query(args));
+  public async update(args: Update.Args): Promise<User> {
+    return await this.prisma.user.update(Update.query(args));
   };
 
   public async obtain(args: Obtain.Args): Promise<Obtain.Result> {
--- server/source/core/dal/users/user.idao.ts
+++ server/source/core/dal/users/user.idao.ts
@@ -1,11 +1,13 @@
+import type { User } from "@prisma/client";
+
 import { Create } from "./queries/create.query";
 import { Delete } from "./queries/delete.query";
 import { Update } from "./queries/update.query";
 import { Obtain } from "./queries/obtain.query";
 
 export interface IUserDao {
-  create(args: Create.Args): Promise<void>;
-  delete(args: Delete.Args): Promise<void>;
-  update(args: Update.Args): Promise<void>;
+  create(args: Create.Args): Promise<User>;
+  delete(args: Delete.Args): Promise<User>;
+  update(args: Update.Args): Promise<User>;
   obtain(args: Obtain.Args): Promise<Obtain.Result>;
 };
