8a13598771e4397a5c71df3a2df0fcfefe867879
roney ruiz <roneyruizrojas@gmail.com>
2026-05-04 19:31:58 -0600

    feat(server): create prisma plugin with daos

---
+++ server/source/database/prisma.plugin.ts
@@ -0,0 +1,15 @@
+import { env } from "@env";
+import { Elysia } from "elysia";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { UserDao } from "@dal/users/user.dao";
+
+export const PrismaPlugin = new Elysia({ name: "prisma.plugin" })
+  .decorate(() => {
+    const adapter = new PrismaPg({ connectionString: env.POSTGRES_URL });
+    const prisma = new PrismaClient({ adapter });
+
+    return {
+      userDao: new UserDao(prisma),
+    };
+  });
