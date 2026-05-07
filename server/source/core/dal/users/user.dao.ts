50663e92bae53908910412ee6993a655b4ffe8ca
roney ruiz <roneyruizrojas@gmail.com>
2026-05-06 23:00:23 -0600

    refac(server): remove delete user query temporary

--- server/source/core/dal/users/user.dao.ts
+++ server/source/core/dal/users/user.dao.ts
@@ -1,18 +1,12 @@
-import type { User } from "@prisma/client";
 import type { IUserDao } from "./user.idao";
 import { PrismaClient } from "@prisma/client";
 
 import { Create } from "./queries/create.query";
-import { Delete } from "./queries/delete.query";
 import { Update } from "./queries/update.query";
 import { Obtain } from "./queries/obtain.query";
 
 export class UserDao implements IUserDao {
   public constructor(private readonly prisma: PrismaClient) { };
-
-  public async delete(args: Delete.Args): Promise<User> {
-    return await this.prisma.user.delete(Delete.query(args));
-  };
 
   public async create(args: Create.Args): Promise<Create.Result> {
     return await this.prisma.user.create(Create.query(args));
--- server/source/core/dal/users/user.idao.ts
+++ server/source/core/dal/users/user.idao.ts
@@ -1,12 +1,8 @@
-import type { User } from "@prisma/client";
-
 import { Create } from "./queries/create.query";
-import { Delete } from "./queries/delete.query";
 import { Update } from "./queries/update.query";
 import { Obtain } from "./queries/obtain.query";
 
 export interface IUserDao {
-  delete(args: Delete.Args): Promise<User>;
   create(args: Create.Args): Promise<Create.Result>;
   update(args: Update.Args): Promise<Update.Result>;
   obtain(args: Obtain.Args): Promise<Obtain.Result | null>;
