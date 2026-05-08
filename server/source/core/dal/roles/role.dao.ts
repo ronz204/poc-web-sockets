dfac90775d2dfd489e613261fc82bf2c4b657f5b
roney ruiz <roneyruizrojas@gmail.com>
2026-05-07 20:42:41 -0600

    chore(server): move role dao contract to main file

--- server/source/core/dal/roles/role.dao.ts
+++ server/source/core/dal/roles/role.dao.ts
@@ -1,8 +1,10 @@
 import type { Scope } from "@prisma/client";
-import type { IRoleDao } from "./role.idao";
 import { PrismaClient } from "@prisma/client";
-
 import { Scopes } from "./queries/scopes.query";
+
+export interface IRoleDao {
+  getScopes(args: Scopes.Args): Promise<Scope[]>;
+};
 
 export class RoleDao implements IRoleDao {
   constructor(private readonly prisma: PrismaClient) { };
--- server/source/core/dal/roles/role.idao.ts
+++
@@ -1,6 +0,0 @@
-import type { Scope } from "@prisma/client";
-import type { Scopes } from "./queries/scopes.query";
-
-export interface IRoleDao {
-  getScopes(args: Scopes.Args): Promise<Scope[]>;
-};
