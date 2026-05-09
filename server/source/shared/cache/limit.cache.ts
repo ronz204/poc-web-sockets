e73ed09dff8a5010adef39abce9556a0f2bea299
roney ruiz <roneyruizrojas@gmail.com>
2026-05-09 16:37:34 -0600

    chore(server): discard native cache limit

--- server/source/shared/cache/limit.cache.ts
+++
@@ -1,9 +0,0 @@
-import type { RedisClient } from "bun";
-
-export interface ILimitCache {
-
-};
-
-export class LimitCache implements ILimitCache {
-  constructor(private readonly redis: RedisClient) {};
-};
